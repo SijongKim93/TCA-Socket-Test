a637c19325cc67e2e15b5d8d5ecced08ad314fda
SijongKim93 <rlatlwhd456@gmail.com>
2025-09-11 13:33:19 +0900

    Initial Commit

---
+++ TCA_Practice/TCA_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_PracticeApp.swift
+//  TCA_Practice
+//
+//  Created by SiJongKim on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TCA_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TCA_PracticeTests/TCA_PracticeTests.swift
@@ -0,0 +1,16 @@
+//
+//  TCA_PracticeTests.swift
+//  TCA_PracticeTests
+//
+//  Created by SiJongKim on 9/11/25.
+//
+
+import Testing
+
+struct TCA_PracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
